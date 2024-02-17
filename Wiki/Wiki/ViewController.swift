a1b6301b1d185af6d8930d8a09866a428ed0e1d5
Max Makarov <mail@mrboss.ru>
2024-02-17 23:05:33 +0300

    init

---
+++ Wiki/Wiki/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Wiki
+//
+//  Created by Max Makarov on 17.02.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
